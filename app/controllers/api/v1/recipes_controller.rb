04b25a61ae3b42cdd5f31c2187e5399417dcefc3
Dan Curtis <dancorpx@gmail.com>
2019-01-18 23:34:09 +0800

    added show action

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -1,5 +1,16 @@
 class Api::V1::RecipesController < Api::V1::BaseController
+    before_action :set_recipe, only: [ :show ]
+    
     def index
         @recipes = Recipe.all  
+    end
+
+    def show
+    end
+
+    private
+    def set_recipe
+        @recipe = Recipe.find(params[:id])
+        render json: @recipe
     end
 end
