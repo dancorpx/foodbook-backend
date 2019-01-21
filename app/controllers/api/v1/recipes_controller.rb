a8101c6a02d5a9489f6350d33368df8a762b8e1a
Dan Curtis <dancorpx@gmail.com>
2019-01-21 10:49:55 +0800

    added update action

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::RecipesController < Api::V1::BaseController
-    before_action :set_recipe, only: [ :show ]
+    protect_from_forgery with: :null_session
+    before_action :set_recipe, only: [ :show, :update ]
     
     def index
         @recipes = Recipe.all  
@@ -8,9 +9,25 @@ class Api::V1::RecipesController < Api::V1::BaseController
     def show
     end
 
+    def update
+        if @recipe.update(recipe_params)
+            render :show
+        else
+            render_error
+        end
+    end
+
     private
     def set_recipe
         @recipe = Recipe.find(params[:id])
-        render json: @recipe
+        # render json: @recipe
+    end
+
+    def recipe_params
+        params.require(:recipe).permit(:title, :method)
+    end
+
+    def render_error
+        render json: { errors: @recipe.errors.full_messages }, status: :unprocessable_entity
     end
 end
