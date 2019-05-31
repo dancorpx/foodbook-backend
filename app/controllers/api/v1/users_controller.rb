5953f626a79651b21478013b844000f8954524f3
Dan Curtis <dancorpx@gmail.com>
2019-05-31 14:26:44 +0800

    minor changes

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -12,7 +12,7 @@ class Api::V1::UsersController < Api::V1::BaseController
     private
 
     def user_params
-        params.require(:user).permit(:id, :avatar)
+        params.require(:user).permit(:id, :avatar, :created_at, :updated_at)
     end
 
     def render_error
