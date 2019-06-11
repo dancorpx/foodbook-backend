3ccebbbc44630029c5dca214418f653244e8166c
Dan Curtis <dancorpx@gmail.com>
2019-06-11 21:58:23 +0800

    added validations to recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,8 @@
 class Recipe < ApplicationRecord
     belongs_to :user
     # has_one :user
+    validates :title, presence: true 
+    validates :method, presence: true
+    validates :image_url, presence: true
+    validates :ingredients, presence: true
 end
